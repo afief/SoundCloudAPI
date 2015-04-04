e445f15c1bc5c83783703723aa85bd15dd2f1087
Afief <ramacyber@gmail.com>
2015-04-04 12:05:31 +0700

    playlist

--- js/SearchModule.js
+++ js/SearchModule.js
@@ -21,7 +21,7 @@ searchMod.controller("SearchController", ["$scope", "$location", "$element", "$h
 
 			$scope.searchResult = [];
 
-			SC.get('/tracks', { q: $scope.searchText, limit: 10}, function(tracks) {
+			SC.get('/tracks', { q: $scope.searchText, limit: 5}, function(tracks) {
 				// get search result
 				for (var i = 0; i < tracks.length; i++) {
 					if (tracks[i].streamable && (tracks[i].policy == "ALLOW")) {
@@ -31,6 +31,7 @@ searchMod.controller("SearchController", ["$scope", "$location", "$element", "$h
 							id: tracks[i].id,
 							url: tracks[i].stream_url + "?client_id=" + client_id,
 							judul: tracks[i].title,
+							duration: Math.floor(tracks[i].duration / 1000),
 							tanggal: tracks[i].created_at,
 							deskripsi: tracks[i].description,
 							thumbnail: tracks[i].artwork_url || tracks[i].user.avatar_url,
@@ -42,8 +43,9 @@ searchMod.controller("SearchController", ["$scope", "$location", "$element", "$h
 				$scope.$apply();
 			});
 
-			$http.get("https://www.googleapis.com/youtube/v3/search?part=id,snippet&q=" + encodeURI($scope.searchText) + "&type=video&order=relevance&maxResults=10&key=" + google_id).
+			$http.get("https://www.googleapis.com/youtube/v3/search?part=id,snippet&q=" + encodeURI($scope.searchText) + "&type=video&order=relevance&maxResults=5&key=" + google_id).
 			success(function(res) {
+				lg(res);
 				if (res.items.length > 0) {
 					for (var i = 0; i < res.items.length; i++) {
 						$scope.searchResult.push({
@@ -52,6 +54,7 @@ searchMod.controller("SearchController", ["$scope", "$location", "$element", "$h
 							id: res.items[i].id.videoId,
 							url: "",
 							judul: res.items[i].snippet.title,
+							duration: 0,
 							deskripsi: res.items[i].snippet.description,
 							tanggal: res.items[i].snippet.publishedAt,
 							thumbnail: res.items[i].snippet.thumbnails.default.url,
@@ -62,10 +65,66 @@ searchMod.controller("SearchController", ["$scope", "$location", "$element", "$h
 				}
 			}).
 			error(function(res) {
-				lg("Error On Youtube");
+				lg("error", res);
 			});
+		}
+	}
 
+	$scope.resultClick = function(res) {
+		if (res.from == "yt") {
+			if (playlist.add("_", res))
+				lg(playlist.current);
+
+			$http.get("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=" + res.id + "&key=" + google_id).
+			success(function(dat) {
+				lg("content details", dat);
+				if (dat.items.length > 0) {
+					res.duration = youtubeTime(dat.items[0].contentDetails.duration);
+					playlist.resetDuration('_');
+				}
+			}).
+			error(function(err) {
+				lg("error", err);
+			});
+		} else if (res.from == "sc") {
+			if (playlist.add("_", res))
+				lg(playlist.current);
+			playlist.resetDuration('_');
 		}
 	}
 
 }]);
+
+
+function youtubeTime(duration) {
+	var a = duration.match(/\d+/g);
+
+	if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
+		a = [0, a[0], 0];
+	}
+
+	if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
+		a = [a[0], 0, a[1]];
+	}
+	if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
+		a = [a[0], 0, 0];
+	}
+
+	duration = 0;
+
+	if (a.length == 3) {
+		duration = duration + parseInt(a[0]) * 3600;
+		duration = duration + parseInt(a[1]) * 60;
+		duration = duration + parseInt(a[2]);
+	}
+
+	if (a.length == 2) {
+		duration = duration + parseInt(a[0]) * 60;
+		duration = duration + parseInt(a[1]);
+	}
+
+	if (a.length == 1) {
+		duration = duration + parseInt(a[0]);
+	}
+	return duration
+}
--- player/PlayerModule.js
+++ player/PlayerModule.js
@@ -1,42 +1,169 @@
 var playerMod = angular.module("PlayerModule", []);
 
 playerMod.service("playlist", function() {
-	this.list = [];
+	this.list = {
+		_: {
+			title: "Playlist Baru",
+			songs: [],
+			totalDuration: 0
+		}
+	};
+	this.current = {};
+	this.currentIndex = -1;
 
-	this.add = function(url, judul, from, meta) {
-		this.list.push({url: url, judul: judul, from: from, meta: meta});
+	this.setAt = function(index) {
+		this.currentIndex = index;
+	}
+	this.getAt = function(index) {
+		return this.current.songs[index];
+	}
+	this.isCurrent = function() {
+		if (this.current.title)
+			return true;
+		return false;
+	}
+	this.resetDuration = function(listID) {
+		this.list[listID].totalDuration = 0;
+		for (var i = 0; i < this.list[listID].songs.length; i++) {
+			this.list[listID].totalDuration += this.list[listID].songs[i].duration;
+		}
+	}
+	this.add = function(listID, obj) {
+		if (this.list[listID] == undefined) {
+			lg("something wrong");
+		} else {
+			var ada = false;
+			for (var i = 0; i < this.list[listID].songs.length; i++) {
+				if (this.list[listID].songs[i].id == obj.id)
+					ada = true;
+			}
+			if (!ada) {
+				this.list[listID].songs.push(obj);
+				this.list[listID].totalDuration += obj.duration;
+				this.current = this.list[listID];
+				return true;
+			}
+			else {
+				lg("song already exist");
+			}
+			return false;
+		}
 	}
 });
 
-playerMod.directive('playerDirective', function () {
+playerMod.directive('playlistDirective', ["$window", function ($window) {
+	return {
+        restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+        replace: true,
+        templateUrl: "player/playlist.html",
+        controller: "playlistController"
+    }
+}]);
+
+var yp;
+playerMod.directive('playerDirective', ["$window", function ($window) {
 	return {
         restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
         replace: true,
         templateUrl: "player/player.html",
+        link: function(scope, element) {
+        	$window.onYouTubeIframeAPIReady = function() {
+        		var yplayer = new YT.Player(element[0].children[0], {
+        			playerVars: {
+        				autoplay: 0,
+        				html5: 1,
+        				modesbranding: 0,
+        				iv_load_policy: 3,
+        				showinfo: 1,
+        				controls: 1,
+        			},
+        			width: element[0].offsetWidth,
+        			height: element[0].offsetWidth * 9 / 16
+        		});
+        		yp = yplayer;
+        		scope.playYoutube = function(id) {
+        			yplayer.getIframe().style.display = "block";
+        			yplayer.loadVideoById(id);
+        			scope.isPlay = true;
+        			scope.playFrom = "yt";
+        		}
+
+        		lg("Youtube API Iframe Ready");
+        	}
+        },
         controller: "PlayerController"
     }
-});
+}]);
 
+var ppl;
+playerMod.controller("playlistController", ["$scope", "$location", "$element", "playlist", function($scope, $location, $element, playlist) {
+	$scope.playlist = playlist;
+	ppl = playlist;
+
+	$scope.timeToStr = function(duration) {
+		var jam = Math.floor(duration / 3600);
+		duration -= jam * 3600;
+		var menit = Math.floor(duration / 60);
+		duration -= menit * 60;
+		var detik = duration;
+
+		var res = "";
+		if (jam > 0)
+			res += jam.toString() + " jam ";
+		if (menit > 0)
+			res += menit.toString() + " menit ";
+		if (detik > 0)
+			res += detik.toString() + " detik ";
+
+		return res; 
+	}
+
+	$scope.dateFormat = function(date) {
+		var a = new Date(date);
+		return a.toLocaleString();
+		//return a.getDate() + "/" + a.getMonth() + "/" + a.getFullYear() + " " + a.getHours() + ":" + a.getMinutes();
+	}
+}]);
 playerMod.controller("PlayerController", ["$scope", "$location", "$element", "playlist", function($scope, $location, $element, playlist) {
 	
-	var video = $element[0].firstElementChild;
+	lg($element);
+
+	var video = $element[0].children[1];
 	video.volume = 0.1;
 	video.src = "";
 
 	$scope.isPlay = false;
 	$scope.playerPlay = function() {
+		lg("PLAY", video.src);
 		video.play();
 	}
 	$scope.playerPause = function() {
 		video.pause();
 	}
+
 	$scope.playUrl = function(url) {
 		video.src = url;
-		video.play();
+		$scope.playerPlay();
+	}
+
+	$scope.playAt = function(index) {
+		var song = playlist.getAt(index);
+		playlist.setAt(index);
+		$scope.playObject(song);
+	}
+
+	$scope.playObject = function(obj) {
+		lg("play object", obj);
+		if (obj.from == "sc") {
+			$scope.playUrl(obj.url);
+		} else if (obj.from = "yt") {
+			$scope.playYoutube(obj.id);
+		}
 	}
 
 	video.addEventListener("play", function() {
 		$scope.isPlay = true;
+		$scope.playFrom = "sc";
 		$scope.$apply();
 	});
 	video.addEventListener("pause", function() {
